3ee896b94da343b1b5ed10ac6ffa674e5bb152b6
Yuri Dias <yuri.dazevedo@gmail.com>
2021-04-14 09:03:32 -0300

    refactor: move EmailValidatorWithError to a jest method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -17,16 +17,6 @@ const makeEmailValidator = (): EmailValidator => {
   return new EmailValidatorStub()
 }
 
-const makeEmailValidatorWithError = (): EmailValidator => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid (email: string): boolean {
-      throw new Error() // Um mock sempre tem que retornar o valor default
-    }
-  }
-
-  return new EmailValidatorStub()
-}
-
 const makeSut = (): Sut => {
   const emailValidatorStub = makeEmailValidator()
   const sut = new SignUpController(emailValidatorStub)
@@ -130,8 +120,10 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 500 if EmailValidator throw', () => {
-    const emailValidatorStub = makeEmailValidatorWithError()
-    const sut = new SignUpController(emailValidatorStub)
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
 
     const httpRequest = {
       body: {
