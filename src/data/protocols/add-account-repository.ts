1ed23ab1de890c3743868ed7aa0cc9ea080c24ff
Yuri Dias <yuri.dazevedo@gmail.com>
2021-04-15 11:25:06 -0300

    feat: ensure DbAddAccount calls AddAccountRepository if correct values

---
+++ src/data/protocols/add-account-repository.ts
@@ -0,0 +1,5 @@
+import { AccountModel, AddAccountModel } from '../useCases/add-account/data-add-account-protocols'
+
+export interface AddAccountRepository{
+  add: (accountData: AddAccountModel) => Promise<AccountModel>
+}
--- src/data/useCases/add-account/data-add-account-protocols.ts
+++ src/data/useCases/add-account/data-add-account-protocols.ts
@@ -1,3 +1,4 @@
 export * from '../../../domain/useCases/addAccount'
 export * from '../../../domain/models/account'
 export * from '../../protocols/encrypter'
+export * from '../../protocols/add-account-repository'
