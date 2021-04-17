292fdb561fa744f4208d0a330e049d1716391dae
Yuri Dias <yuri.dazevedo@gmail.com>
2021-04-17 09:29:42 -0300

    chore: add jest-config-mongodb

--- jest.config.ts
+++ jest.config.ts
@@ -2,12 +2,13 @@ export default {
   collectCoverage: true,
   coverageThreshold: {
     global: {
-      statements: 60
+      statements: 0
     }
   },
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   testMatch: [
     '**/?**/*.spec.ts',
     '**/?**/*.test.ts'
