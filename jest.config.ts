ee160f5cb93f62906b2acd4c7935844f93ee6e60
Yuri Dias <yuri.dazevedo@gmail.com>
2021-04-17 11:26:07 -0300

    fix: jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,18 @@
 export default {
-  collectCoverage: true,
+  /* collectCoverage: true,
   coverageThreshold: {
     global: {
       statements: 0
     }
-  },
+  }, */
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   testMatch: [
-    '**/?**/*.spec.ts',
-    '**/?**/*.test.ts'
+    '**/*.spec.ts',
+    '**/*.test.ts'
   ],
   transform: {
     '.+||.ts$': 'ts-jest'
