62bfe2b2baa91ce1bef7bf15fbe30da72fc1e747
Yuri Dias <yuri.dazevedo@gmail.com>
2021-04-19 11:24:55 -0300

    fix: mongodb connect

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,7 +1,7 @@
 import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
-  client: null as unknown as MongoClient,
+  client: undefined as unknown as MongoClient,
 
   async connect (uri: string): Promise<void> {
     this.client = await MongoClient.connect(uri, {
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://docker:docker@localhost:27017/clean-node-api?authSource=admin',
   port: process.env.PORT || 5050
 }
