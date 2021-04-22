7c849ac8c79fe4f0ffa1c21cda7f96277d591c31
Yuri Dias <yuri.dazevedo@gmail.com>
2021-04-22 08:42:36 -0300

    test: ensure LogMongoRepository creates an error log on sucess

--- src/infra/db/mongodb/log-repository/log.spec.ts
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -1,6 +1,9 @@
+import { Collection } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { LogMongoRepository } from './log'
 
 describe('LogError Repository', () => {
+  let errorCollection: Collection
   beforeAll(async () => {
     if (process.env.MONGO_URL) { await MongoHelper.connect(process.env.MONGO_URL) }
   })
@@ -10,11 +13,14 @@ describe('LogError Repository', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('error')
-    await accountCollection.deleteMany({})
+    errorCollection = await MongoHelper.getCollection('error')
+    await errorCollection.deleteMany({})
   })
 
-  test('should create an error log on sucess', () => {
-
+  test('should create an error log on sucess', async () => {
+    const sut = new LogMongoRepository()
+    await sut.logError('any_error')
+    const count = await errorCollection.countDocuments()
+    expect(count).toBe(1)
   })
 })
