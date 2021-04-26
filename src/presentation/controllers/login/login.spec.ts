4938e02ee1b73f4e8a04e559b4d898e299b7174d
Yuri Dias <yuri.dazevedo@gmail.com>
2021-04-26 08:39:40 -0300

    test: ensure LoginController returns 4000 if no email is provided

---
+++ src/presentation/controllers/login/login.spec.ts
@@ -0,0 +1,18 @@
+import { MissingParamsError } from '../../error'
+import { BadRequest } from '../../helpers/http-helper'
+import { HttpRequest } from '../../protocols'
+import { LoginController } from './login'
+
+describe('Login controller test', () => {
+  test('should return bad request if no email is provider', async () => {
+    const sut = new LoginController()
+    const httpRequest: HttpRequest = {
+      body: {
+        password: 'any123'
+      }
+    }
+
+    const response = await sut.handle(httpRequest)
+    expect(response).toEqual(BadRequest(new MissingParamsError('email')))
+  })
+})
