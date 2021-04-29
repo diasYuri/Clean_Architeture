593a233fee06d77d7ef4573bf59a1ebde877522f
Yuri Dias <yuri.dazevedo@gmail.com>
2021-04-29 11:47:46 -0300

    feat: ensure DbAuthentication calls UpdateAcessTokenRepository with correct values

--- src/data/useCases/authentication/db-authentication.ts
+++ src/data/useCases/authentication/db-authentication.ts
@@ -2,16 +2,24 @@ import { Authentication, AuthenticationModel } from '../../../domain/useCases/au
 import { HashComparer } from '../../protocols/criptography/hash-compare'
 import { TokenGenerator } from '../../protocols/criptography/token-generator'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
+import { UpdateAcessTokenRepository } from '../../protocols/db/update-acess-token-repository'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   private readonly hashComparer: HashComparer
   private readonly tokenGeneration: TokenGenerator
+  private readonly updateAcessTokenRepository: UpdateAcessTokenRepository
 
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparer: HashComparer, tokenGenerator: TokenGenerator) {
+  constructor (
+    loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    hashComparer: HashComparer,
+    tokenGenerator: TokenGenerator,
+    updateAcessTokenRepository: UpdateAcessTokenRepository
+  ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
     this.hashComparer = hashComparer
     this.tokenGeneration = tokenGenerator
+    this.updateAcessTokenRepository = updateAcessTokenRepository
   }
 
   async auth (authentication: AuthenticationModel): Promise<string> {
@@ -20,6 +28,7 @@ export class DbAuthentication implements Authentication {
       const isValid = await this.hashComparer.compare(authentication.password, account.password)
       if (isValid) {
         const acessToken = await this.tokenGeneration.generate(account.id)
+        await this.updateAcessTokenRepository.update(account.id, acessToken)
         return acessToken
       }
     }
